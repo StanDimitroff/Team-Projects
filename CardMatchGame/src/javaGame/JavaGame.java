8eb7059657df16bedf7573d815e7730c6d5b5ba0
StanDimitroff <standimitroff@gmail.com>
2014-05-29 16:10:21 +0300

    GUI game implemented in Java

---
+++ CardMatchGame/src/javaGame/JavaGame.java
@@ -0,0 +1,20 @@
+package javaGame;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+public class JavaGame extends JFrame {
+
+//	public static void main(String[] args) {
+
+//		CardButton go = new CardButton();
+//		go.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		go.setSize(600, 620);
+//		go.setResizable(true);
+//		go.setVisible(true);
+//		go.setTitle("Card Match Game");
+//		go.setLayout(new GridLayout(4, 4, 4, 4));
+
+	}
+
